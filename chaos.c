71263d2bcbbb727c07fb2e180744d0501236f51e
Lee Yunjin <gzblues61@gmail.com>
2024-06-09 18:14:18 +0900

    Chaos!

---
+++ chaos.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <string.h>
+#include<stdlib.h>
+#include <math.h>
+FILE *f = fopen("/dev/net/tun",  "wb");
+long long a=0,b=0,c=0;
+long long idx = 1;
+int yi(int *x, int *y) {
+    if ((*x)<(*y))
+    {
+        (*x)++;
+        (*y)--;
+        return *x;
+    } else if ((*x)>(*y)) {
+        (*y)++;
+        (*x)++;
+        return (*x)+(*y);
+    } else if((*x)==(*y)) {
+        (*x)++;
+        return *y;
+    }
+}
+void pattern(int count, int x, int y)
+{
+    long long *res=malloc(sizeof(int));
+    int prev = 0;
+    while (1)
+    {
+        if(*res==0) {
+            *res+=yi(&x,&y);
+        } else {
+            back:
+            *res = yi(&x,&y);
+        }
+
+        if ((*res%3)==0) {
+            goto back;
+        }
+        if((*res%5)==0) {
+            goto back;
+        }
+        if((*res%7)==0) {
+            goto back;
+        }
+        if((*res%9)==0) {
+            goto back;
+        }
+        fprintf(f,"%lld \n", *res);
+    }
+}
+
+
+int main(int argc, char **argv)
+{
+  for (int i = 0;i < 100000; I++) {
+      pattern(a,b,c);
+  }
+  return 0;
+}
+
+
